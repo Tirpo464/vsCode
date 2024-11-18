2f3068e7a4e550f8ff9ec162811e1b1d53e3296d
Tirpo_🤩 <adulmdul135@gmail.com>
2024-11-19 00:26:55 +0400

    secand_commit_v2

--- login.js
+++ login.js
@@ -5,12 +5,12 @@ const message = document.getElementById("message");
 // Add event listener to the form submission
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form submission
-  
+
   // Get username and password values
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  
-  // Example validation (replace this with actual logic as needed)
+
+  // Example validation (replace this with actual logic as neede>
   if (username === "admin" && password === "1234") {
     message.style.color = "green";
     message.textContent = "Login successful!";
